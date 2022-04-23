ae8ae74ff7a68774c73470650eed8545561a9775
FAZAN4K <71427947+FAZAN4K@users.noreply.github.com>
2022-04-23 18:42:05 +1000

    Update faq.php

--- faq.php
+++ faq.php
@@ -5,9 +5,9 @@
     <section class="page-body faq">
         <div class="container">
             <h1 class="title">самые популярные вопросы</h1>
-            <!-- <ul class='faq-list'>
+            <ul class='faq-list'>
                <li>ТЕКСТ</li>
-            </ul> -->
+            </ul>
         </div>
     </section>
     <script src="assets/js/register-validation.js"></script>
